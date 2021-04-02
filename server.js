54a279807adbfe84af624ee03d721453e3ef79fe
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-04-02 16:17:41 +0300

    final requirment

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ const PORT = process.env.PORT;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const PARKS_API_KEY = process.env.PARKS_API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const YELP_API_KEY =process.env.YELP_API_KEY;
+
 const app = express();
 app.use(cors());
 
 app.get("/location", handleLocationrequest);
 app.get("/weather", handleWeatherrequest);
 app.get("/parks", handleParkrequest);
+app.get('/movies',handlemovies);
+app.get('/yelp',handleYelp);
 
 function handleLocationrequest(req, res) {
   const city = req.query.city;
@@ -73,6 +78,63 @@ function handleParkrequest(req, res) {
     });
 }
 
+
+function handlemovies(req,res){
+  let arrayOFMovies=[];
+  // const url=`https://api.themoviedb.org/3/movie/550?api_key=da735d1206c20c177c63a737bdb7678e`;
+  // const url =`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&query=${request.query.city}`;
+  const url =`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${req.query.search_query}`;
+  superagent.get(url).then(data=> {
+    let movies=data.body.results;
+    movies.forEach(element=>{
+      arrayOFMovies.push(new Movies(element));
+    });
+    res.send(arrayOFMovies);
+    console.log(arrayOFMovies)
+  }).catch((err)=> {
+    console.log('ERROR IN movies API');
+    console.log(err);
+  });
+}
+
+
+function handleYelp(req,res){
+ 
+  let arrYelps=[];
+  let lat = req.query.latitude;
+  let lon = req.query.longitude;
+const url =`https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lon}`
+superagent.get(url).set(`Authorization`,`Bearer${YELP_API_KEY}`).then(data=>{
+  let yelpsData=data.body.businesses;
+  yelpsData.forEach(element=>{
+    arrYelps.push(new Yelps(element))
+  })
+  res.send(arrYelps);
+}).catch((err)=> {
+  console.log('ERROR IN movies API');
+  console.log(err);
+});
+}
+
+function Yelps(data){
+  this.name=data.name;
+  this.url=data.url;
+  this.price=data.price;
+  this.rating=data.rating;
+  this.image_url=data.image_url;
+}
+
+function Movies(data){
+  this.title=data.title;
+  this.overview=data.overview;
+  this.average_votes=data.vote_average;
+  this.total_votes=data.vote_count;
+  this.image_url=`https://image.tmdb.org/t/p/w500${data.poster_path}`;
+  this.popularity=data.popularity;
+  this.released_on=data.release_date;
+}
+
+
 function NewPark(data) {
   this.name = data.name;
   this.address = `${data.addresses[0].line1} ${data.addresses[0].city} ${data.addresses[0].stateCode} ${data.addresses[0].postalCode}`;
@@ -82,7 +144,7 @@ function NewPark(data) {
 
   
 function Location(city, data) {
-  this.searchQuery1 = city;
+  this.search_query = city;
   this.formatted_query = data.display_name;
   this.latitude = data.lat;
   this.longitude = data.lon;
